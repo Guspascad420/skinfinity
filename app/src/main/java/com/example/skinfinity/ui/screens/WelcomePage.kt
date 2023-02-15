07e7c99e3305a642e1b4c92b1d80c6fa8e8e387e
Guspascad420 <Paacad@gmail.com>
2023-02-15 11:09:40 +0700

    Added WelcomePage composable

---
+++ app/src/main/java/com/example/skinfinity/ui/screens/WelcomePage.kt
@@ -0,0 +1,82 @@
+package com.example.skinfinity.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skinfinity.R
+import com.example.skinfinity.ui.theme.OpenSans
+import com.example.skinfinity.ui.theme.SkinfinityTheme
+
+@Composable
+fun WelcomePage() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            Modifier
+                .width(220.dp)
+                .padding(top = 65.dp)) {
+            Text(
+                "Welcome to Skinfinity!",
+                fontFamily = OpenSans,
+                fontWeight = FontWeight.Bold,
+                fontSize = 23.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable._633419_1),
+            contentDescription = "null",
+            modifier = Modifier
+                .size(360.dp)
+                .padding(vertical = 20.dp)
+        )
+        Column(Modifier.width(320.dp)) {
+            Text(
+                "Lorem ipsum dolor sit amet consectetur.",
+                fontFamily = OpenSans,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                "Lorem ipsum dolor sit amet consectetur. Id sed est mi viverra et. " +
+                        "Fusce enim lectus eget risus fusce ut accumsan. Et proin nullam dui mi donec " +
+                        "hac ornare egestas sagittis. Nunc semper urna pellentesque quisque facilisis.",
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(Modifier.height(30.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFFFF9999),
+                contentColor = Color.White
+            ),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(width = 170.dp, height = 50.dp)
+        ) {
+            Text("GET STARTED")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    SkinfinityTheme {
+        WelcomePage()
+    }
+}
