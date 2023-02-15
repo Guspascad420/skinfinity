5194a110f0f9979603c5b42ad3ca6f959fcfaf97
Guspascad420 <Paacad@gmail.com>
2023-02-15 11:09:59 +0700

    Added Opensans font family into type

--- app/src/main/java/com/example/skinfinity/ui/theme/Type.kt
+++ app/src/main/java/com/example/skinfinity/ui/theme/Type.kt
@@ -2,11 +2,19 @@ package com.example.skinfinity.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.skinfinity.R
 
 // Set of Material typography styles to start with
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.open_sans_bold, FontWeight.Bold),
+    Font(R.font.open_sans)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
