57883957622ca68c5ebd968d063262f6d89bad4e
Guspascad420 <Paacad@gmail.com>
2023-02-17 13:22:18 +0700

    Added Login Page route

--- app/src/main/java/com/example/skinfinity/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/skinfinity/ui/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.example.skinfinity.ui.navigation
 sealed class Screen(val route: String) {
     object WelcomePage: Screen("welcome")
     object SignUp: Screen("sign-up")
+    object Login: Screen("login")
 }
