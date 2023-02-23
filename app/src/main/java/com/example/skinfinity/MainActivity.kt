e45eb8ab5c43021fe42ec15536f705f6076b9849
Guspascad420 <Paacad@gmail.com>
2023-02-23 16:13:11 +0700

    Added Splash Screen

--- app/src/main/java/com/example/skinfinity/MainActivity.kt
+++ app/src/main/java/com/example/skinfinity/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.skinfinity.ui.navigation.SkinfinityApp
 import com.example.skinfinity.ui.theme.SkinfinityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             SkinfinityTheme {
                 // A surface container using the 'background' color from the theme
