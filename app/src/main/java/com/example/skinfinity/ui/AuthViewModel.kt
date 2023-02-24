6c9cdc9f7d57aac255ae5ef885e364a43002ce9d
Guspascad420 <Paacad@gmail.com>
2023-02-24 20:22:04 +0700

    Added firebase auth methods

--- app/src/main/java/com/example/skinfinity/ui/AuthViewModel.kt
+++ app/src/main/java/com/example/skinfinity/ui/AuthViewModel.kt
@@ -10,15 +10,17 @@ import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 private const val TAG = "Auth"
 
 sealed interface AuthUiState {
-    data class Success(val user: FirebaseUser?) : AuthUiState
+    object Success : AuthUiState
     object Error : AuthUiState
     object Loading : AuthUiState
+    object Default : AuthUiState
 }
 
 class AuthViewModel : ViewModel() {
@@ -27,22 +29,65 @@ class AuthViewModel : ViewModel() {
     var password by mutableStateOf(TextFieldValue(""))
     private val auth: FirebaseAuth = Firebase.auth
 
-    var authUiState: AuthUiState by mutableStateOf(AuthUiState.Loading)
+    var authUiState: AuthUiState by mutableStateOf(AuthUiState.Default)
         private set
 
     fun signUpClick() {
         viewModelScope.launch {
+            authUiState = AuthUiState.Loading
             auth.createUserWithEmailAndPassword(email.text, password.text)
                 .addOnCompleteListener { task ->
-                    authUiState = if (task.isSuccessful) {
+                    if (task.isSuccessful) {
                         Log.d(TAG, "createUserWithEmail:success")
-                        AuthUiState.Success(auth.currentUser)
+                        val user = auth.currentUser
+                        val profileUpdates = userProfileChangeRequest {
+                            displayName = fullName.text
+                        }
+                        user!!.updateProfile(profileUpdates)
+                            .addOnCompleteListener { updateTask ->
+                                if (updateTask.isSuccessful) {
+                                    Log.d(TAG, "User profile updated.")
+                                }
+                            }
+                        authUiState = AuthUiState.Success
                     } else {
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        authUiState = AuthUiState.Error
+                    }
+                }
+        }
+
+    }
+
+    fun sendEmail(user: FirebaseUser? = auth.currentUser) {
+        viewModelScope.launch {
+            authUiState = AuthUiState.Loading
+            user!!.sendEmailVerification()
+                .addOnCompleteListener { task ->
+                    authUiState = if (task.isSuccessful) {
+                        Log.d(TAG, "Email sent.")
+                        Firebase.auth.signOut()
+                        AuthUiState.Success
+                    } else {
                         AuthUiState.Error
                     }
                 }
         }
     }
 
+    fun loginClick() {
+        viewModelScope.launch {
+            authUiState = AuthUiState.Loading
+            auth.signInWithEmailAndPassword(email.text, password.text)
+                .addOnCompleteListener { task ->
+                    authUiState = if (task.isSuccessful) {
+                        Log.d(TAG, "Successfully Sign In User")
+                        AuthUiState.Success
+                    } else {
+                        Log.w(TAG, "signInUser:failure", task.exception)
+                        AuthUiState.Error
+                    }
+            }
+        }
+    }
 }
