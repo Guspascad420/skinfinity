0b93f9fb21c3731a3a77f1a7c3fb6804eecb0dd1
Guspascad420 <Paacad@gmail.com>
2023-03-28 15:19:26 +0700

    Added mock todo data instances

--- app/src/main/java/com/example/skinfinity/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/skinfinity/ui/HomeViewModel.kt
@@ -5,10 +5,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.skinfinity.R
+import com.example.skinfinity.data.Todo
+import java.text.SimpleDateFormat
 import java.time.LocalTime
+import java.util.*
 
 class HomeViewModel : ViewModel() {
     var dayIcon by mutableStateOf(R.drawable.fa6_solid_cloud_moon)
+    val todoLists = listOf(
+        Todo("Judul To do list", "Lorem ipsum dolor sit amet consectetur. " +
+                "Risus sodales posuere sagittis sed blandit id accumsan.",
+            LocalTime.parse("07:00:00"), true),
+        Todo("Judul To do list", "Lorem ipsum dolor sit amet consectetur. " +
+                "Risus sodales posuere sagittis sed blandit id accumsan.",
+            LocalTime.parse("13:00:00"), false),
+        Todo("Judul To do list", "Lorem ipsum dolor sit amet consectetur. " +
+                "Risus sodales posuere sagittis sed blandit id accumsan.",
+            LocalTime.parse("19:00:00"), false)
+    )
+    val newsImage = listOf(
+        R.drawable.rectangle_32, R.drawable.rectangle_33,
+        R.drawable.rectangle_34, R.drawable.rectangle_35
+    )
 
     fun dayPartGreeting(): String {
         val time = LocalTime.now()
@@ -37,3 +55,9 @@ class AppScreenIcon(
     val activeIconId: Int = 0,
     val onClick: () -> Unit = { }
 )
+
+fun getCurrentDay(): String {
+    val sdf = SimpleDateFormat("EEEE")
+    val d = Date()
+    return sdf.format(d)
+}
