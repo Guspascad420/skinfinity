4e0cdee5d24350d17fa451f30801e525627b98ec
Guspascad420 <Paacad@gmail.com>
2023-03-28 15:18:53 +0700

    Added app routes

--- app/src/main/java/com/example/skinfinity/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/skinfinity/ui/navigation/Screen.kt
@@ -5,7 +5,11 @@ sealed class Screen(val route: String) {
     object SignUp: Screen("sign-up")
     object Login: Screen("login")
     object EmailVerification: Screen("verify-email")
-    object Home: Screen("Home")
-    object Profile: Screen("Profile")
+    object Home: Screen("home")
+    object Profile: Screen("profile")
     object TodoList: Screen("todo")
+    object AddTodo: Screen("add-todo")
+    object Consult: Screen("consult")
+    object Goals: Screen("goals")
+    object AddGoals: Screen("add-goals")
 }
